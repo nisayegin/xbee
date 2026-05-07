48a5f2542a8acae782575278449aa2b5b8bd800a
nisayegin <nisa.yegin@bahcesehir.edu.tr>
2026-05-07 22:52:31 +0300

    Create crc16.h

---
+++ crc16.h
@@ -0,0 +1,11 @@
+#ifndef CRC16_H
+#define CRC16_H
+ 
+#include <stdint.h>
+#include <stddef.h>
+ 
+uint16_t crc16_ccitt(const uint8_t *data, size_t len);
+uint16_t crc16_ccitt_update(uint16_t crc, const uint8_t *data, size_t len);
+ 
+#endif
+ 
